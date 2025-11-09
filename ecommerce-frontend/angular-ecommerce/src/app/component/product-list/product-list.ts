040064b76bc0b1854c2782c087457f7bf21e330f
ElisabethSchramm <59915326+ElisabethSchramm@users.noreply.github.com>
2025-11-09 21:21:14 +0100

    feat(frontend): adjust product-list component

--- ecommerce-frontend/angular-ecommerce/src/app/component/product-list/product-list.ts
+++ ecommerce-frontend/angular-ecommerce/src/app/component/product-list/product-list.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule, CurrencyPipe } from '@angular/common';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product';
+
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [CommonModule, CurrencyPipe], 
   templateUrl: './product-list.html',
   styleUrl: './product-list.css',
 })
-export class ProductList {
+
+export class ProductListComponent implements OnInit {
+
+  products: Product[] = [];
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.listProducts();
+  }
+
+  listProducts() {
+    this.productService.getProductList().subscribe(
+      data => {
+        this.products = data;
+      }
+    )
+  }
 
 }
