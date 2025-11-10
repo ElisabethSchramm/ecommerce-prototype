223dd6b431b831df990378d5e410367fc4047bfe
ElisabethSchramm <59915326+ElisabethSchramm@users.noreply.github.com>
2025-11-10 17:26:25 +0100

    feat(cart): add shopping cart items CRUD

--- ecommerce-frontend/angular-ecommerce/src/app/app.routes.ts
+++ ecommerce-frontend/angular-ecommerce/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list';
 import { ProductDetailsComponent } from './components/product-details/product-details';
+import { CartDetailsComponent } from './components/card-details/card-details';
 
 export const routes: Routes = [
+  { path: 'cart-details', component: CartDetailsComponent },
   { path: 'products/:id', component: ProductDetailsComponent },
   { path: 'search/:keyword', component: ProductListComponent },
   { path: 'category/:id', component: ProductListComponent },
--- ecommerce-frontend/angular-ecommerce/src/app/components/cart-status/cart-status.ts
+++ ecommerce-frontend/angular-ecommerce/src/app/components/cart-status/cart-status.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/services/cart';
 import { CommonModule, CurrencyPipe } from '@angular/common';
+import { CartDetailsComponent } from '../card-details/card-details';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cart-status',
-  imports: [CommonModule, CurrencyPipe],
+  imports: [CommonModule, CurrencyPipe, RouterLink],
   templateUrl: './cart-status.html',
   styleUrl: './cart-status.css',
 })
