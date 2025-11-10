6118fe4b96dd4e00810bbcbe4eae4ab84215e701
ElisabethSchramm <59915326+ElisabethSchramm@users.noreply.github.com>
2025-11-10 04:20:23 +0100

    feat(app): integrate UI templates and implement product search by category

--- ecommerce-frontend/angular-ecommerce/src/app/app.ts
+++ ecommerce-frontend/angular-ecommerce/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ProductListComponent } from './component/product-list/product-list';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { ProductListComponent } from './components/product-list/product-list';
+import { ProductCategoryMenu } from './components/product-category-menu/product-category-menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductListComponent], 
+  imports: [RouterOutlet, ProductCategoryMenu],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
